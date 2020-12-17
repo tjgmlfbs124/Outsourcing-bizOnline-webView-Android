ce93fccbb5b125cd5b2170ce5c5e2019c1546cf3
tjgmlfbs124 <tjgmlfbs124@gmail.com>
2020-12-17 13:09:29 +0900

    init

--- app/src/main/java/com/example/biz_online_webview/MainActivity.java
+++ app/src/main/java/com/example/biz_online_webview/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.biz_online_webview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    int count = 0;
+    Timer timer;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timer = new Timer();
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run() {
+                if(count > 1) {
+                    timer.cancel();
+                    Intent intent = new Intent(MainActivity.this, WebviewActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+                else
+                    count++;
+            }
+
+        };
+        timer.schedule(tt, 0, 2000);
     }
 }
---
+++ app/src/main/java/com/example/biz_online_webview/WebviewActivity.java
@@ -0,0 +1,40 @@
+package com.example.biz_online_webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebviewActivity extends AppCompatActivity {
+    private WebView webView;
+    private String url = "http://192.168.0.15:14000/";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
